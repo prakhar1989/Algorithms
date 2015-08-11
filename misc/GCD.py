416d261aca116f9cf7baf5217140d993d05a27d0
Vedang Mehta <vedang.mehta36@gmail.com>
2015-08-11 18:15:26 +0530

    Update GCD.py

--- misc/GCD.py
+++ misc/GCD.py
@@ -15,10 +15,7 @@ Algorithm :
 """
 
 def greatest_common_divisor(x,y):
-    if(y == 0):
-        return x
-    else:
-        return greatest_common_divisor(y,x%y)
+    return x if y == 0 else greatest_common_divisor(y,x%y)
 
 if __name__ == "__main__":
     print(greatest_common_divisor(20,25))
