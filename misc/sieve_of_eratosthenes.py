f30f609910ebd888a47b37992884ca55fe360964
Prakhar Srivastav <prakhar1989@gmail.com>
2015-09-09 12:41:05 -0400

    refactored sieve of eratosthenes

--- misc/sieve_of_eratosthenes.py
+++ misc/sieve_of_eratosthenes.py
@@ -1,5 +1,4 @@
 """
-
 Implementation of Sieve of Eratosthenes algorithm to generate all the primes upto N.
 
 Reference : https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
@@ -7,23 +6,20 @@ Reference : https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 Algorithm :
 * We have a list of numbers from 1 to N.
 * Initially, all the numbers are marked as primes.
-* We go to every prime number in the list (<= N ^ 1/2) and mark all the multiples of this prime number which are bigger than the number itself as non-primes.
-
+* We go to every prime number in the list (<= N ^ 1/2) and mark all the multiples 
+  of this prime number which are bigger than the number itself as non-primes.
 """
 
 from math import sqrt,ceil
 
-def calculate_primes(n):
-    bool_array = [True] * (n+1)
-    bool_array[0] = False
-    bool_array[1] = False
-    upper_bound = ceil(sqrt(n))
-    for i in range(2,upper_bound):
-        if bool_array[i]:
-            for j in range(i*i,n+1,i):
-                bool_array[j] = False
-    prime_array = [i for i in range(n+1) if bool_array[i]]
-    return prime_array
+def generate_primes(n):
+    bool_array = [False, False] + [True] * n              # start with all values as True, except 0 and 1
+    for i in range(2, int(ceil(sqrt(n)))):                # only go to till square root of n
+        if bool_array[i]:                                 # if the number is marked as prime
+            for j in range(i*i,n+1,i):                    # iterate through all its multiples
+                bool_array[j] = False                     # and mark them as False
+    primes = [i for i in range(n+1) if bool_array[i]]     # return all numbers which are marked as True
+    return primes
 
 if __name__ == "__main__":
-    print(calculate_primes(50))
+    print(generate_primes(50))
