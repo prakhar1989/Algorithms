2f2cdf3f6b82fe761050629f37304ef1c869592b
Shank <shashanksharma21@gmail.com>
2016-09-26 22:46:42 +0530

    modify Queue to use collections.deque instead of a list

--- lists/queue.py
+++ lists/queue.py
@@ -1,6 +1,12 @@
+from collections import deque
+
+
 class Queue(object):
+    """Wrapper around collections.deque to provide the api consistent with
+    a Queue"""
+
     def __init__(self):
-        self.items = []
+        self.items = deque()
 
     def __str__(self):
         return ("Queue of size: %d" % len(self.items))
@@ -9,11 +15,10 @@ class Queue(object):
         return len(self.items) == 0
 
     def enqueue(self, item):
-        self.items.insert(0, item)
+        self.items.append(item)
 
     def dequeue(self):
-        return self.items.pop()
+        return self.items.popleft()
 
     def size(self):
         return len(self.items)
-
